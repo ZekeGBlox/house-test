262f8567986627c16187a4007fb9c808a7c90fb1
ZekeGBlox <157068979+ZekeGBlox@users.noreply.github.com>
2026-01-12 18:48:48 +0000

    some cottage house looking shit bro

---
+++ Cozy Cottage/manifest.lua
@@ -0,0 +1,37 @@
+return {
+    house = {
+        building_type = "micro_2023",
+        unique = "house_{ad8e24fb-3236-4dbc-abbf-a0620e085a82}",
+        active_addons = {
+        },
+        house_pos = Vector3.new(-6000,4000,3000),
+        house_id = 68,
+    },
+    textures = {
+        Room3 = {
+            floors = "expensivepurplecarpet",
+            walls = "pink",
+        },
+        Room1 = {
+            floors = "woodenplanks",
+            walls = "bluestripes",
+        },
+        Room2 = {
+            floors = "checkers",
+            walls = "white",
+        },
+    },
+    metadata = {
+        totalFurniture = 3314,
+        originalOwner = "Javonainttrippin",
+        date = "2026-01-12 18:24:36",
+        savedBy = "Imnoob62826",
+        totalChunks = 2,
+        version = "2.0",
+        chunkSize = 2000,
+        chunked = true,
+        timestamp = 1768242276,
+        savedByUserId = 1340750543,
+        totalValue = 41016,
+    },
+}
